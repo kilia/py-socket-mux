a463052d4a1159e9f8b4ddb50dc709a55ac2235e
kilia <wuyu.zju@163.com>
2016-06-23 19:08:56 +0800

    several test, still very slow even in localhost, sleep cost too much

--- udp_client.py
+++ udp_client.py
@@ -3,18 +3,18 @@ import gevent
 from gevent import socket, queue
 from sys import stdout, stderr
 
-address = ('104.224.140.44', 9000)
-#address = ('127.0.0.1', 9000)
+#address = ('104.224.140.44', 9000)
+address = ('127.0.0.1', 9000)
 
 sock = socket.socket(type=socket.SOCK_DGRAM)
 sock.connect(address)
 #print('Sending %s bytes to %s:%s' % ((len(message), ) + address))
 
-M = 4
+M = 200
 
-def sender(sock, packets, multi, speed_limit = 500):
+def sender(sock, packets, multi, speed_limit = 1000):
     p = [multi for i in xrange(packets)]
-    batch = int(100/M)
+    batch = int(1)
     starttime = time.time()
     while len(p) > 0:
         t0 = time.time()
@@ -24,8 +24,8 @@ def sender(sock, packets, multi, speed_limit = 500):
             sock.send(struct.pack('<i', p.pop()))
         dt = time.time() - t0
         dt = batch * 1./speed_limit - dt
-        if dt > 0:
-            #print 'sleeping', dt
+        if dt > 0.0:
+            #stderr.write('sleeping %.2f sec\n' % dt)
             gevent.sleep(dt)
     stderr.write('sent all in %.2f sec\n' % (time.time() - starttime))
 
@@ -55,7 +55,7 @@ def counter(q):
             break
         stdout.write(data)
 
-packets = 20000
+packets = 100000
 q = queue.Queue()
 greenlets = [
     gevent.spawn(sender,sock, packets/M, M),
--- udp_server.py
+++ udp_server.py
@@ -5,7 +5,7 @@ from gevent.server import DatagramServer
 from gevent import queue
 import gevent
 
-q = queue.Queue()
+q = queue.Queue(16)
 
 class EchoServer(DatagramServer):
     def __init__(self, *args, **kwargs):
@@ -17,8 +17,10 @@ class EchoServer(DatagramServer):
         global q
         assert(len(data)==4)
         count, = struct.unpack('<i', data)
-        if count > 64: count = 64
-        if count < 1: count = 1
+        #if count > 64: count = 64
+        #if count < 1: count = 1
+        gevent.sleep(0.1)
+        print 'sending %d packets' % count
         for i in xrange(count):
             data = struct.pack('<i', self._seq) + self._payload
             self._seq += 1
@@ -26,15 +28,16 @@ class EchoServer(DatagramServer):
             q.put((self.socket, data, address))
 
 
-def sender(limit = 8 * 1024 * 1024):
+def sender(limit = 15 * 1024 * 1024):
     global q
     print 'sender started'
     window_size = 128
-    window = deque(maxlen = window_size)
+    window = deque(maxlen = window_size * 2)
     t = time.time()
     for _ in xrange(window_size):
         window.append((t, 0))
     bytes_in_window = 0
+    comp_t = 0
     while True:
         # send a packet
         socket, data, addr = q.get()
@@ -47,11 +50,29 @@ def sender(limit = 8 * 1024 * 1024):
         bytes_in_window += len(data)
         window.append((t, len(data)))
 
+        if len(window) != window_size:
+            print 'window size = %d' % len(window)
+
+        #print bytes_in_window
+
         # check if we need to slow down
-        dt = t - oldest[1]
+        dt = t - oldest[0]
+        #print dt
         sleeptime = bytes_in_window*1.0/limit - dt
+
+        if sleeptime < comp_t:
+            comp_t -= sleeptime
+            sleeptime = 0
+        else:
+            sleeptime -= comp_t
+            comp_t = 0            
+
         if sleeptime > 0:
-            gevent.sleep(sleeptime)
+            t = time.time()
+            gevent.sleep(0.001)
+            rt = time.time() - t
+            comp_t += (rt - sleeptime)/25
+            print 'want to sleep %.4f sec, acctually %.4f sec' % (sleeptime, rt)
 
 if __name__ == '__main__':
     print('Receiving datagrams on :9000')
